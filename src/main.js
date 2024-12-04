34204e0c3faf3b357d2b7582ef85fc487bf8091f
Tiegist <tiegistgirma40@gmail.com>
2024-12-04 17:59:16 +0300

    first change

--- src/main.js
+++ src/main.js
@@ -4,5 +4,5 @@ import router from './router'
 import store from './store'
 import './assets/tailwind.css'
 import 'primeicons/primeicons.css'
-
+// some changes in main js
 createApp(App).use(store).use(router).mount('#app')
